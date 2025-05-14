4a8be95bb06ad4d3afb2ba63e6c7ab5f6d3bda58
Anna <mynameisannanas@gmail.com>
2025-05-14 21:51:21 +0200

    test basename

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,9 @@ const router = createBrowserRouter([
       }
     ]
   }
-]);
+], {
+  basename: '/horizondubai'
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
