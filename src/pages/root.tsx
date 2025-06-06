3ef9f2bfb7a84d92c2cbd02f751cdaf178c94b19
Anna <mynameisannanas@gmail.com>
2025-06-06 21:59:19 +0200

    fix: deploy

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
-import Header from '@components/h/h';
-import Footer from '@components/f/f';
+import Header from '@components/header/header';
+import Footer from '@components/footer/footer';
 
 export default function Root() {
   return (
